bba36fbc1d7a982fb2ff0939e6a87e99d1b4d521
Ali Aizad <aliaizad72@gmail.com>
2024-03-10 23:32:57 +0800

    Able to create an array enlisting the maximum knight vector movements

--- knights.rb
+++ knights.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'matrix'
+
 # Contains the relationship between each 'box' with it's appropriate move
 class KnightMoves
   attr_reader :vertices, :edges
@@ -11,12 +13,9 @@ class KnightMoves
 
   def create_vertices
     arr = []
-
     8.times do |row|
       8.times do |column|
-        coordinate = [row]
-        coordinate.push(column)
-        arr.push(coordinate)
+        arr.push(Vector[row, column])
       end
     end
     arr
@@ -24,6 +23,19 @@ class KnightMoves
 
   def create_edges
   end
-end
 
-p KnightMoves.new.vertices.length
+  def all_movements
+    movements = []
+    [1, 2, -1, -2].permutation(2) do |perm|
+      movements.push(Vector.elements(perm)) if perm.map(&:abs).sum == 3
+    end
+    movements
+  end
+end
+knight = KnightMoves.new
+vertices = knight.vertices
+movements = knight.all_movements.map { |v| v + vertices[24] }
+movements = movements.filter do |vectors|
+  vectors.all? { |v| v >= 0 && v < 8 }
+end
+p movements
